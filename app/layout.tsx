601697263ea81660991239488f656d3fc7ad6265
Lev1reG <derentanaphan@mail.ugm.ac.id>
2024-10-18 15:51:13 +0700

    feat: add authentication ui

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { Sidebar } from "@/components/layout/sidebar";
 import { FollowBar } from "@/components/layout/follow-bar";
+import { LoginModal } from "@/components/modals/login-modal";
+import { RegisterModal } from "@/components/modals/register-modal";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,7 +22,9 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={`${inter.className} antialiased`}
-      >
+      > 
+        <RegisterModal />
+        <LoginModal />
         <div className="h-screen bg-black">
           <div className="container h-full mx-auto xl:px-30 max-w-6xl">
             <div className="grid grid-cols-4 h-full">
--- components/layout/sidebar-tweet-button.tsx
+++ components/layout/sidebar-tweet-button.tsx
@@ -1,14 +1,21 @@
 "use client";
 
+import { useLoginModal } from "@/hooks/use-login-modal";
 import { useRouter } from "next/navigation"
+import { useCallback } from "react";
 import { FaFeather } from "react-icons/fa";
 
 export const SidebarTweetButton = () => {
   const router = useRouter();
+  const loginModal = useLoginModal();
+
+  const onClick = useCallback(() => {
+    loginModal.onOpen(); 
+  }, [loginModal])
 
   return (
     <div
-      onClick={() => router.push("/")}
+      onClick={onClick}
     >
       <div className="mt-6 lg:hidden rounded-full h-14 w-14 p-4 flex items-center justify-center bg-sky-500 hover:bg-opacity-80 transititon cursor-pointer">
         <FaFeather size={24} color="white" />
