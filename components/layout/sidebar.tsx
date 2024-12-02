c188dc68b93943fbc5260997c82f1e82e669dd69
Lev1reG <derentanaphan@mail.ugm.ac.id>
2024-12-02 20:15:19 +0700

    fix: sidebar profile routing

--- components/layout/sidebar.tsx
+++ components/layout/sidebar.tsx
@@ -1,16 +1,15 @@
-import {
-  BsBellFill,
-  BsHouseFill
-} from "react-icons/bs";
+import { BsBellFill, BsHouseFill } from "react-icons/bs";
 import { FaUser } from "react-icons/fa";
 import { SidebarLogo } from "@/components/layout/sidebar-logo";
 import { SidebarItem } from "@/components/layout/sidebar-item";
 import { BiLogOut } from "react-icons/bi";
 import { SidebarTweetButton } from "@/components/layout/sidebar-tweet-button";
 import { signOut, useSession } from "next-auth/react";
+import useCurrentUser from "@/hooks/use-current-user";
 
 export const Sidebar = () => {
   const session = useSession();
+  const { data: currentUser } = useCurrentUser();
 
   const items = [
     {
@@ -22,41 +21,39 @@ export const Sidebar = () => {
       label: "Notification",
       href: "/notification",
       icon: BsBellFill,
-      auth: true
+      auth: true,
     },
     {
       label: "Profile",
-      href: "user/123",
+      href: `/users/${currentUser?.id}`,
       icon: FaUser,
-      auth: true
-    }
-  ]
+      auth: true,
+    },
+  ];
   return (
     <div className="col-span-1 h-full pr-4 md:pr-6">
       <div className="flex flex-col items-end">
         <div className="space-y-2 lg:w-[230px]">
           <SidebarLogo />
-          {
-            items.map((item) => (
-              <SidebarItem
-                key={item.href}
-                href={item.href}
-                label={item.label}
-                icon={item.icon}
-                auth={item?.auth}
-              />
-            ))
-          }
+          {items.map((item) => (
+            <SidebarItem
+              key={item.href}
+              href={item.href}
+              label={item.label}
+              icon={item.icon}
+              auth={item?.auth}
+            />
+          ))}
           {session.status === "authenticated" && (
             <SidebarItem
               onClick={() => signOut()}
               icon={BiLogOut}
               label="Logout"
-            />)
-          }
+            />
+          )}
           <SidebarTweetButton />
         </div>
       </div>
     </div>
   );
-}
+};
